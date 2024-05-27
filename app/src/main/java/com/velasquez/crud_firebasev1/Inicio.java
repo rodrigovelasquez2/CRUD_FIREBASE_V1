403d8b23c792d8f79d2dd1bebb72020fa9fdee37
rvelasquezq <rodrigovelas2@gmail.com>
2024-05-26 22:24:32 -0500

    Se añadio un timer para que pasara de un activity a otro en pocos segundos: ref: https://www.youtube.com/watch?v=2JGm67leD7g

---
+++ app/src/main/java/com/velasquez/crud_firebasev1/Inicio.java
@@ -0,0 +1,43 @@
+package com.velasquez.crud_firebasev1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Inicio extends AppCompatActivity {
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Inicio.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }//Fin run
+        }, 1000);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }//Fin onCreate
+
+
+}//Fin Inicio
